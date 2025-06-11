43e104e5882a56d0ff8b53648f3b14121f391e95
ABUBAKAR ATHAR <2021cs533@student.uet.edu.pk>
2025-06-11 09:27:38 +0500

    Create Maintenance.js

---
+++ src/components/pages/Maintenance.js
@@ -0,0 +1,14 @@
+// src/components/pages/Maintenance.js
+import React from 'react';
+import './Maintenance.css'; // Optional CSS
+
+const Maintenance = () => {
+  return (
+    <div className="maintenance-container">
+      <h1>🚧 Website Under Maintenance</h1>
+      <p>We're currently doing some work. Please check back soon!</p>
+    </div>
+  );
+};
+
+export default Maintenance;
